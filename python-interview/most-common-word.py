89c46dcc804acde5b436867789d50e4a341394fd
honeybeat1 <honeybeat1@gmail.com>
2022-11-16 00:07:35 +0900

    add: counter

--- python-interview/most-common-word.py
+++ python-interview/most-common-word.py
@@ -1,11 +1,13 @@
+from collections import Counter
+
 def mostCommonWord(para, ban):
     table = para.maketrans('', '', ',.')
     para = para.translate(table)
-    word_list = list(para.lower().split(' '))
-    word_list = [w for w in word_list if w not in ban]
-    print(word_list)
+    word_list = [w for w in list(para.lower().split(' ')) if w not in ban]
+    most_common_word = Counter(word_list).most_common(1)[0][0]
+    return most_common_word
 
 if __name__ == '__main__':
-    paragraph = "Bob hit a ball, the hit BALL flew far after it was hit."
+    PARA = "Bob hit a ball, the hit BALL flew far after it was hit."
     banned = ['hit']
-    mostCommonWord(paragraph, banned)
+    print(mostCommonWord(PARA, banned))
